47af531d9cb9de27d0d4917efcdb0b598a11de7a
Matthew Sevey <mjsevey@gmail.com>
2021-02-05 12:17:06 -0700

    Fix tests

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>SkyTemplates coming soon.</p>
+        <p>SkyTemplates coming soon</p>
         <a
           className="App-link"
           href="https://siasky.net"
