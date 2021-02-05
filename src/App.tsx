cbcd9496b89e7f86c6a3c431b42609d0cbbef5eb
Matthew Sevey <mjsevey@gmail.com>
2021-02-05 12:10:02 -0700

    Install prettier and run

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
-import logo from './skynet_logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./skynet_logo.svg";
+import "./App.css";
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>SkyTemplates coming soon.</p>
-				<a className="App-link" href="https://siasky.net" target="_blank" rel="noopener noreferrer">
-					Learn about Skynet
-				</a>
-			</header>
-		</div>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>SkyTemplates coming soon.</p>
+        <a
+          className="App-link"
+          href="https://siasky.net"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn about Skynet
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
